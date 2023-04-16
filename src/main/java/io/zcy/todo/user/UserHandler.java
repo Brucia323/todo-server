d232d111b1096f8a73046acf0ebda473cbb8250a
周晨阳 <1448517833@qq.com>
2023-04-16 15:15:04 +0800

    Create UserHandler.java

---
+++ src/main/java/io/zcy/todo/user/UserHandler.java
@@ -0,0 +1,75 @@
+package io.zcy.todo.user;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import jakarta.annotation.Resource;
+import java.util.Map;
+import java.util.Optional;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+public class UserHandler {
+  @Resource private UserService service;
+
+  public Mono<ServerResponse> login(ServerRequest request) {
+    return request
+        .bodyToMono(UserDTO.class)
+        .flatMap(
+            userDTO ->
+                service
+                    .getUserByEmail(userDTO)
+                    .flatMap(
+                        user -> {
+                          if (user == null) {
+                            return Mono.empty();
+                          }
+                          boolean checkpw =
+                              BCrypt.checkpw(userDTO.getPassword(), user.getPasswordHash());
+                          if (!checkpw) {
+                            return Mono.empty();
+                          }
+                          String token =
+                              JWT.create()
+                                  .withClaim("id", user.getId())
+                                  .sign(Algorithm.HMAC256("todo"));
+                          Map<String, String> response =
+                              Map.of("name", user.getName(), "token", token);
+                          return ServerResponse.status(HttpStatus.CREATED)
+                              .body(response, Map.class);
+                        }));
+  }
+
+  public Mono<ServerResponse> getUserById(ServerRequest request) {
+    return Mono.just(Integer.valueOf(request.pathVariable("id")))
+        .flatMap(service::getUserById)
+        .map(UserDTO::new)
+        .flatMap(userDTO -> ServerResponse.ok().body(userDTO, UserDTO.class));
+  }
+
+  public Mono<ServerResponse> createUser(ServerRequest request) {
+    return request
+        .bodyToMono(UserDTO.class)
+        .flatMap(service::createUser)
+        .map(UserDTO::new)
+        .flatMap(userDTO -> ServerResponse.status(HttpStatus.CREATED).body(userDTO, UserDTO.class));
+  }
+
+  public Mono<ServerResponse> updateUser(ServerRequest request) {
+    return request
+        .bodyToMono(UserDTO.class)
+        .flatMap(service::updateUser)
+        .map(UserDTO::new)
+        .flatMap(userDTO -> ServerResponse.ok().body(userDTO, UserDTO.class));
+  }
+
+  public Mono<ServerResponse> deleteUser(ServerRequest request) {
+    return Mono.just(Integer.valueOf(request.pathVariable("id")))
+        .flatMap(service::deleteUser)
+        .then(ServerResponse.noContent().build());
+  }
+}
