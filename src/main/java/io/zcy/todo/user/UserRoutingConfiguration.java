1421f77a3cf1963792c5c183f6e61f611cd7bbc6
zhouchenyang <zhouchenyang@infuturespace.com>
2023-04-17 10:58:03 +0800

    Create UserRoutingConfiguration.java

---
+++ src/main/java/io/zcy/todo/user/UserRoutingConfiguration.java
@@ -0,0 +1,23 @@
+package io.zcy.todo.user;
+
+import static io.zcy.todo.Util.ACCEPT_JSON;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration(proxyBeanMethods = false)
+public class UserRoutingConfiguration {
+  @Bean
+  public RouterFunction<ServerResponse> monoRouterFunction(UserHandler userHandler) {
+    return route()
+        .GET("/user/{id}", ACCEPT_JSON, userHandler::getUserById)
+        .POST("/user", ACCEPT_JSON, userHandler::createUser)
+        .PUT("/user/{id}", ACCEPT_JSON, userHandler::updateUser)
+        .DELETE("/user/{id}", ACCEPT_JSON, userHandler::deleteUser)
+        .POST("/login", ACCEPT_JSON, userHandler::login)
+        .build();
+  }
+}
