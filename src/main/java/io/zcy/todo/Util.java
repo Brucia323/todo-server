5bfc1e99842b299e534ae43867822cd69067ecbb
zhouchenyang <zhouchenyang@infuturespace.com>
2023-04-17 10:58:11 +0800

    Update Util.java

--- src/main/java/io/zcy/todo/Util.java
+++ src/main/java/io/zcy/todo/Util.java
@@ -1,14 +1,18 @@
 package io.zcy.todo;
 
-import org.springframework.web.reactive.function.server.ServerRequest;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 
 import java.util.Optional;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicate;
+import org.springframework.web.reactive.function.server.ServerRequest;
 
 public class Util {
+  public static final RequestPredicate ACCEPT_JSON = accept(MediaType.APPLICATION_JSON);
+
   public static Optional<String> getTokenFrom(ServerRequest request) {
-    String authorization =
-        request.headers().header("Authorization").get(0);
-    if (authorization!=null && authorization.toLowerCase().startsWith("bearer ")) {
+    String authorization = request.headers().header("Authorization").get(0);
+    if (authorization != null && authorization.toLowerCase().startsWith("bearer ")) {
       return Optional.of(authorization.substring(7));
     }
     return Optional.empty();
