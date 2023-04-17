ce0b2ec4fb2b86f45c96831ee9318f254def6bf6
周晨阳 <1448517833@qq.com>
2023-04-17 21:04:52 +0800

    Create todo_record.sql

---
+++ src/main/resources/todo_record.sql
@@ -0,0 +1,19 @@
+-- Table: public.todo_record
+
+-- DROP TABLE IF EXISTS public.todo_record;
+
+CREATE TABLE IF NOT EXISTS public.todo_record
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    user_id integer NOT NULL,
+    todo_id integer NOT NULL,
+    current_amount integer NOT NULL,
+    create_time timestamp without time zone NOT NULL,
+    update_time timestamp without time zone NOT NULL,
+    CONSTRAINT todo_record_pk PRIMARY KEY (id)
+    )
+
+    TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.todo_record
+    OWNER to postgres;
