8e340ec70718336b3cb7aa849227dc342253d49b
ZHOU CHENYANG <zhouchenyang@infuturespace.com>
2023-04-18 18:05:13 +0800

    Update todos.sql

--- src/main/resources/todos.sql
+++ src/main/resources/todos.sql
@@ -6,17 +6,17 @@ CREATE TABLE IF NOT EXISTS public.todos
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     user_id integer NOT NULL,
+    name text COLLATE pg_catalog."default" NOT NULL,
     begin_time timestamp without time zone,
     planned_end_time timestamp without time zone,
     actual_end_time timestamp without time zone,
     current_amount integer,
     total_amount integer,
-    description "char",
+    description text COLLATE pg_catalog."default",
     create_time timestamp without time zone NOT NULL,
     update_time timestamp without time zone NOT NULL,
-    name "char" NOT NULL,
     CONSTRAINT todo_pk PRIMARY KEY (id)
-    )
+)
 
     TABLESPACE pg_default;
 
