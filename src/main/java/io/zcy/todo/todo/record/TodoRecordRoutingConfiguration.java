9b4fb3ec09bce560a04fb0d3066b4d5acf87255a
ZHOU CHENYANG <zhouchenyang@infuturespace.com>
2023-04-18 09:42:30 +0800

    Create TodoRecordRoutingConfiguration.java

---
+++ src/main/java/io/zcy/todo/todo/record/TodoRecordRoutingConfiguration.java
@@ -0,0 +1,17 @@
+package io.zcy.todo.todo.record;
+
+import static io.zcy.todo.Util.ACCEPT_JSON;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration(proxyBeanMethods = false)
+public class TodoRecordRoutingConfiguration {
+  @Bean
+  public RouterFunction<ServerResponse> TodoRecordRouterFunction(TodoRecordHandler todoRecordHandler) {
+    return route().POST("/todo/{id}", ACCEPT_JSON, todoRecordHandler::createTodoRecord).build();
+  }
+}
