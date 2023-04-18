d4f5f1e4c3d3502f398563ba393e9157f51f71d3
ZHOU CHENYANG <zhouchenyang@infuturespace.com>
2023-04-18 18:05:16 +0800

    Update users.sql

--- src/main/resources/users.sql
+++ src/main/resources/users.sql
@@ -5,15 +5,15 @@
 CREATE TABLE IF NOT EXISTS public.users
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    name "char" NOT NULL,
-    email "char" NOT NULL,
-    password_hash "char" NOT NULL,
-    time_per_week "char",
+    name text COLLATE pg_catalog."default" NOT NULL,
+    email text COLLATE pg_catalog."default" NOT NULL,
+    password_hash text COLLATE pg_catalog."default" NOT NULL,
+    time_per_week text COLLATE pg_catalog."default",
     create_time timestamp without time zone NOT NULL,
     update_time timestamp without time zone NOT NULL,
     CONSTRAINT user_pk PRIMARY KEY (id),
     CONSTRAINT email UNIQUE (email)
-    )
+)
 
     TABLESPACE pg_default;
 
