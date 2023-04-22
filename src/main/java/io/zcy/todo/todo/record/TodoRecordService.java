dccdeaf1718d46b45c8815e87946d8fa77bab9dc
周晨阳 <1448517833@qq.com>
2023-04-22 17:05:59 +0800

    Update TodoRecordService.java

--- src/main/java/io/zcy/todo/todo/record/TodoRecordService.java
+++ src/main/java/io/zcy/todo/todo/record/TodoRecordService.java
@@ -1,16 +1,20 @@
 package io.zcy.todo.todo.record;
 
+import io.zcy.todo.todo.Todo;
+import io.zcy.todo.todo.TodoDTO;
 import io.zcy.todo.todo.TodoRepository;
+import io.zcy.todo.todo.TodoService;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @Service
 public class TodoRecordService {
   @Resource private TodoRecordRepository repository;
 
-  @Resource private TodoRepository parentRepository;
+  @Resource private TodoService parentService;
 
   public Flux<TodoRecord> getTodoRecordsByTodoId(Integer todoId) {
     return repository.findByTodoId(todoId);
@@ -23,6 +27,15 @@ public class TodoRecordService {
   public Mono<TodoRecord> createTodoRecord(
       TodoRecordDTO todoRecordDTO, Integer userId, Integer todoId) {
     TodoRecord todoRecord = new TodoRecord(userId, todoId, todoRecordDTO.getCurrentAmount());
+    parentService
+        .getTodoById(todoId)
+        .publishOn(Schedulers.boundedElastic())
+        .map(TodoDTO::new)
+        .flatMap(
+            todoDTO -> {
+              todoDTO.setCurrentAmount(todoRecord.getCurrentAmount());
+              return parentService.updateTodo(todoDTO);
+            });
     return repository.save(todoRecord);
   }
 }
