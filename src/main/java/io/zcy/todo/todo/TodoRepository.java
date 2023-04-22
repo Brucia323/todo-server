56dfa9901266ad27fbe85bba34e34dff3bbdd0f9
周晨阳 <1448517833@qq.com>
2023-04-22 17:06:05 +0800

    Update TodoRepository.java

--- src/main/java/io/zcy/todo/todo/TodoRepository.java
+++ src/main/java/io/zcy/todo/todo/TodoRepository.java
@@ -3,11 +3,8 @@ package io.zcy.todo.todo;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @Repository
 public interface TodoRepository extends ReactiveCrudRepository<Todo, Integer> {
-  Flux<Todo> findByUserId(Integer userId);
-
-  Mono<Todo> updateById(Integer id);
+  Flux<Todo> findByUserIdOrderByIdDesc(Integer userId);
 }
