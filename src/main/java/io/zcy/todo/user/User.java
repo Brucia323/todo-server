3d97b6c4b2f33e315ef62dc3580d03b76df06127
ZHOU CHENYANG <zhouchenyang@infuturespace.com>
2023-04-23 17:18:27 +0800

    Update User.java

--- src/main/java/io/zcy/todo/user/User.java
+++ src/main/java/io/zcy/todo/user/User.java
@@ -2,10 +2,16 @@ package io.zcy.todo.user;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("users")
+@Getter
+@Setter
+@ToString
 public class User {
   @Id private Integer id;
   private String name;
@@ -40,85 +46,5 @@ public class User {
     this.timePerWeek = timePerWeek;
     this.createTime = createTime;
     this.updateTime = updateTime;
-  }
-
-  public Integer getId() {
-    return id;
-  }
-
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPasswordHash() {
-    return passwordHash;
-  }
-
-  public void setPasswordHash(String passwordHash) {
-    this.passwordHash = passwordHash;
-  }
-
-  public String getTimePerWeek() {
-    return timePerWeek;
-  }
-
-  public void setTimePerWeek(String timePerWeek) {
-    this.timePerWeek = timePerWeek;
-  }
-
-  public LocalDateTime getCreateTime() {
-    return createTime;
-  }
-
-  public void setCreateTime(LocalDateTime createTime) {
-    this.createTime = createTime;
-  }
-
-  public LocalDateTime getUpdateTime() {
-    return updateTime;
-  }
-
-  public void setUpdateTime(LocalDateTime updateTime) {
-    this.updateTime = updateTime;
-  }
-
-  @Override
-  public String toString() {
-    return "User{"
-        + "id="
-        + id
-        + ", name='"
-        + name
-        + '\''
-        + ", email='"
-        + email
-        + '\''
-        + ", passwordHash='"
-        + passwordHash
-        + '\''
-        + ", timePerWeek='"
-        + timePerWeek
-        + '\''
-        + ", createTime="
-        + createTime
-        + ", updateTime="
-        + updateTime
-        + '}';
   }
 }
