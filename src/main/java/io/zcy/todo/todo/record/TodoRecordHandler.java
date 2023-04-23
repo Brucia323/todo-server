89775cb991fe132a6b6807cf35130fbf96408f94
ZHOU CHENYANG <zhouchenyang@infuturespace.com>
2023-04-23 14:06:33 +0800

    Update TodoRecordHandler.java

--- src/main/java/io/zcy/todo/todo/record/TodoRecordHandler.java
+++ src/main/java/io/zcy/todo/todo/record/TodoRecordHandler.java
@@ -5,6 +5,7 @@ import static io.zcy.todo.Util.getTokenFrom;
 import com.auth0.jwt.JWT;
 import jakarta.annotation.Resource;
 import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -12,6 +13,7 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 @Component
+@Slf4j
 public class TodoRecordHandler {
   @Resource private TodoRecordService service;
 
@@ -22,6 +24,7 @@ public class TodoRecordHandler {
     }
     Integer userId = JWT.decode(token.get()).getClaim("id").asInt();
     Integer todoId = Integer.valueOf(request.pathVariable("id"));
+    log.info("用户 id: {} 更新任务: {} 进度", userId, todoId);
     Mono<TodoRecordDTO> todoRecordDTO =
         request
             .bodyToMono(TodoRecordDTO.class)
