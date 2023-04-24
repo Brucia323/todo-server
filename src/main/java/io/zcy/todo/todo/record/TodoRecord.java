16c74a171af1ca6bcc63f7eddd30253e4d8e7fb5
ZHOU CHENYANG <zhouchenyang@infuturespace.com>
2023-04-24 17:51:22 +0800

    Update TodoRecord.java

--- src/main/java/io/zcy/todo/todo/record/TodoRecord.java
+++ src/main/java/io/zcy/todo/todo/record/TodoRecord.java
@@ -15,29 +15,34 @@ public class TodoRecord {
   @Id private Integer id;
   private Integer userId;
   private Integer todoId;
-  private Integer currentAmount;
+  private Integer amount;
   private LocalDateTime createTime = LocalDateTime.now();
   private LocalDateTime updateTime = LocalDateTime.now();
 
   public TodoRecord() {}
 
-  public TodoRecord(Integer userId, Integer todoId, Integer currentAmount) {
+  public TodoRecord(Integer userId, Integer todoId, Integer amount) {
     this.userId = userId;
     this.todoId = todoId;
-    this.currentAmount = currentAmount;
+    this.amount = amount;
+  }
+
+  public TodoRecord(Integer amount, LocalDateTime createTime) {
+    this.amount = amount;
+    this.createTime = createTime;
   }
 
   public TodoRecord(
       Integer id,
       Integer userId,
       Integer todoId,
-      Integer currentAmount,
+      Integer amount,
       LocalDateTime createTime,
       LocalDateTime updateTime) {
     this.id = id;
     this.userId = userId;
     this.todoId = todoId;
-    this.currentAmount = currentAmount;
+    this.amount = amount;
     this.createTime = createTime;
     this.updateTime = updateTime;
   }
