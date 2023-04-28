8f554fa9e4e8f72083bb0f2c8d4ca2230fae9a0d
ZHOU CHENYANG <zhouchenyang@infuturespace.com>
2023-04-28 17:43:16 +0800

    Update Lstm.java

--- src/main/java/io/zcy/todo/dl4j/Lstm.java
+++ src/main/java/io/zcy/todo/dl4j/Lstm.java
@@ -1,8 +1,14 @@
 package io.zcy.todo.dl4j;
 
+import io.zcy.todo.todo.record.TodoRecordService;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-public class Lstm {}
+public class Lstm {
+  @Resource private TodoRecordService todoRecordService;
+
+  public void lstm() {}
+}
