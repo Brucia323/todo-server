28d0dae67e169f7c75ef55cde2c5196df9ac9744
周晨阳 <1448517833@qq.com>
2023-05-04 10:40:40 +0800

    Create AccountRepository.java

---
+++ src/main/java/io/zcy/todo/account/AccountRepository.java
@@ -0,0 +1,10 @@
+package io.zcy.todo.account;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+
+@Repository
+public interface AccountRepository extends ReactiveCrudRepository<Account, Integer> {
+  Mono<Account> findByEmail(String email);
+}
