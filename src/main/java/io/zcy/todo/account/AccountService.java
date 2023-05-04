44921b60b3bb43ff2bbaaeb7b609838694c062c7
周晨阳 <1448517833@qq.com>
2023-05-04 10:40:44 +0800

    Create AccountService.java

---
+++ src/main/java/io/zcy/todo/account/AccountService.java
@@ -0,0 +1,73 @@
+package io.zcy.todo.account;
+
+import jakarta.annotation.Resource;
+import java.time.LocalDateTime;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@Slf4j
+public class AccountService {
+  @Resource private AccountRepository repository;
+
+  public Mono<Account> getUserById(Integer id) {
+    return repository.findById(id);
+  }
+
+  public Mono<Account> getUserByEmail(String email) {
+    return repository.findByEmail(email);
+  }
+
+  public Mono<Account> createUser(AccountDTO accountDTO) {
+    return repository
+        .findByEmail(accountDTO.getEmail())
+        .defaultIfEmpty(new Account())
+        .flatMap(
+                account -> {
+              if (account.getId() != null) {
+                log.info("【{}】已注册", account.getEmail());
+                return Mono.error(new RuntimeException("该邮箱已注册"));
+              }
+              String passwordHash = BCrypt.hashpw(accountDTO.getPassword(), BCrypt.gensalt(10));
+              account = new Account(accountDTO.getName(), accountDTO.getEmail(), passwordHash);
+              log.info("正在注册: {}", account);
+              return repository.save(account);
+            });
+  }
+
+  public Mono<Account> updateUser(AccountDTO accountDTO) {
+    return repository
+        .findById(accountDTO.getId())
+        .map(
+                account -> {
+              log.info("【{}】正在更新信息", account.getId());
+              String password = accountDTO.getPassword();
+
+              account.setName(accountDTO.getName());
+              account.setEmail(accountDTO.getEmail());
+              account.setTimePerWeek(accountDTO.getTimePerWeek());
+              account.setUpdateTime(LocalDateTime.now());
+
+              if (password != null) {
+                String passwordHash = BCrypt.hashpw(password, BCrypt.gensalt(10));
+                account.setPasswordHash(passwordHash);
+              }
+
+              return account;
+            })
+        .flatMap(repository::save);
+  }
+
+  public Mono<Void> deleteUser(Integer id) {
+    return repository.deleteById(id);
+  }
+
+  public Mono<String> generateCaptcha() {
+    double v = Math.random() * 9000 + 1000;
+    long round = Math.round(v);
+    log.info("验证码是【{}】", round);
+    return Mono.just(String.valueOf(round));
+  }
+}
