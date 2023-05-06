e58e607b54a358323564a0488ed2880035cd84cb
周晨阳 <1448517833@qq.com>
2023-05-06 09:57:55 +0800

    Update TodoServerApplication.java

--- src/main/java/io/zcy/todo/TodoServerApplication.java
+++ src/main/java/io/zcy/todo/TodoServerApplication.java
@@ -1,16 +1,12 @@
 package io.zcy.todo;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.tensorflow.TensorFlow;
 
 @SpringBootApplication
-@Slf4j
 public class TodoServerApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(TodoServerApplication.class, args);
-    log.info("TensorFlow version: {}", TensorFlow.version());
   }
 }
