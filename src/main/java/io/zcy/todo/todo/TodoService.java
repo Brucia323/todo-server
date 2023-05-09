c139de12d9259bcc605f2e030d9c1f448a52dcd4
周晨阳 <1448517833@qq.com>
2023-05-09 21:17:07 +0800

    Update TodoService.java

--- src/main/java/io/zcy/todo/todo/TodoService.java
+++ src/main/java/io/zcy/todo/todo/TodoService.java
@@ -1,8 +1,16 @@
 package io.zcy.todo.todo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.zcy.todo.Util;
+import io.zcy.todo.account.Account;
+import io.zcy.todo.account.AccountService;
 import jakarta.annotation.Resource;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -12,6 +20,7 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class TodoService {
   @Resource private TodoRepository repository;
+  @Resource private AccountService accountService;
 
   public Mono<Todo> getTodoById(Integer id) {
     return repository.findById(id);
@@ -30,7 +39,6 @@ public class TodoService {
             todoDTO.getPlannedEndDate(),
             todoDTO.getTotalAmount(),
             todoDTO.getDescription());
-    log.info("任务正在创建: {}", todo);
     return repository.save(todo);
   }
 
@@ -48,7 +56,6 @@ public class TodoService {
                 todo.setActualEndDate(LocalDate.now());
               }
               todo.setUpdateTime(LocalDateTime.now());
-              log.info("任务正在被更新: {}", todo);
               return todo;
             })
         .flatMap(repository::save);
@@ -56,5 +63,32 @@ public class TodoService {
 
   public Mono<Void> deleteTodo(Integer id) {
     return repository.deleteById(id);
+  }
+
+  public Flux<Todo> getProgram(Integer userId) {
+    return accountService
+        .getUserById(userId)
+        .map(Account::getTimePerWeek)
+        .map(
+            time -> {
+              ObjectMapper mapper = new ObjectMapper();
+              try {
+                List<Util.TimeObject> timeObject = mapper.readValue(time, new TypeReference<>() {});
+                return timeObject.stream().map(Util.TimeObject::day).toArray(String[]::new);
+              } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+              }
+            })
+        .flatMapMany(Flux::fromArray)
+        .flatMap(
+            day -> {
+              int dayOfWeek = LocalDate.now().getDayOfWeek().getValue();
+              if (Integer.parseInt(day) == dayOfWeek % 7) {
+                return getTodosByUser(userId)
+                    .filter(todo -> todo.getActualEndDate() == null)
+                    .sort(Comparator.comparing(Todo::getPlannedEndDate));
+              }
+              return Flux.empty();
+            });
   }
 }
