3db7430f2538ab1537a85dd180a123eb98ca3240
周晨阳 <1448517833@qq.com>
2023-05-16 15:52:26 +0800

    Update AccountRepository.java

--- src/main/java/io/zcy/todo/account/AccountRepository.java
+++ src/main/java/io/zcy/todo/account/AccountRepository.java
@@ -2,6 +2,7 @@ package io.zcy.todo.account;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+
 import reactor.core.publisher.Mono;
 
 @Repository
