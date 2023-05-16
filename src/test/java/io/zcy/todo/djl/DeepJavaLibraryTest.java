4a227a1211eacfdcee2d91c477cf230230402cd4
周晨阳 <1448517833@qq.com>
2023-05-16 15:52:59 +0800

    Update DeepJavaLibraryTest.java

--- src/test/java/io/zcy/todo/djl/DeepJavaLibraryTest.java
+++ src/test/java/io/zcy/todo/djl/DeepJavaLibraryTest.java
@@ -1,16 +1,19 @@
 package io.zcy.todo.djl;
 
-import ai.djl.training.TrainingResult;
-import ai.djl.translate.TranslateException;
-import jakarta.annotation.Resource;
 import java.io.IOException;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import ai.djl.training.TrainingResult;
+import ai.djl.translate.TranslateException;
+import jakarta.annotation.Resource;
+
 @SpringBootTest
 class DeepJavaLibraryTest {
-  @Resource private DeepJavaLibrary deepJavaLibrary;
+  @Resource
+  private DeepJavaLibrary deepJavaLibrary;
 
   @Test
   void lstm() throws TranslateException, IOException {
