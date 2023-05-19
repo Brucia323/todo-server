6c4b2fa3dd8018a065edca585ad8a57580e61894
周晨阳 <1448517833@qq.com>
2023-05-19 20:31:18 +0800

    Update AccountRoutingConfiguration.java

--- src/main/java/io/zcy/todo/account/AccountRoutingConfiguration.java
+++ src/main/java/io/zcy/todo/account/AccountRoutingConfiguration.java
@@ -1,25 +1,27 @@
 package io.zcy.todo.account;
 
-import static io.zcy.todo.Util.ACCEPT_JSON;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static io.zcy.todo.Util.ACCEPT_JSON;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration(proxyBeanMethods = false)
 public class AccountRoutingConfiguration {
   @Bean
   public RouterFunction<ServerResponse> monoRouterFunction(AccountHandler accountHandler) {
     return route()
-        .GET("/user", ACCEPT_JSON, accountHandler::getUserById)
-        .GET("/user/email/{email}", ACCEPT_JSON, accountHandler::getUserByEmail)
-        .GET("/user/captcha", ACCEPT_JSON, accountHandler::generateCaptcha)
-        .POST("/user", ACCEPT_JSON, accountHandler::createUser)
-        .PUT("/user/{id}", ACCEPT_JSON, accountHandler::updateUser)
-        .DELETE("/user/{id}", ACCEPT_JSON, accountHandler::deleteUser)
-        .POST("/login", ACCEPT_JSON, accountHandler::login)
-        .build();
+      .GET("/user", ACCEPT_JSON, accountHandler::getUserById)
+      .GET("/user/email/{email}", ACCEPT_JSON, accountHandler::getUserByEmail)
+      .GET("/user/captcha", ACCEPT_JSON, accountHandler::generateCaptcha)
+      .GET("/password", RequestPredicates.queryParam("password", t -> true), accountHandler::checkPassword)
+      .POST("/user", ACCEPT_JSON, accountHandler::createUser)
+      .PUT("/user/{id}", ACCEPT_JSON, accountHandler::updateUser)
+      .DELETE("/user/{id}", ACCEPT_JSON, accountHandler::deleteUser)
+      .POST("/login", ACCEPT_JSON, accountHandler::login)
+      .build();
   }
 }
