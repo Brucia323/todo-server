d6b00508d7f6b25a53b7df19293e3bf0b729277c
周晨阳 <1448517833@qq.com>
2023-05-23 15:41:49 +0800

    Update AccountService.java

--- src/main/java/io/zcy/todo/account/AccountService.java
+++ src/main/java/io/zcy/todo/account/AccountService.java
@@ -1,12 +1,13 @@
 package io.zcy.todo.account;
 
-import jakarta.annotation.Resource;
-import lombok.extern.slf4j.Slf4j;
+import java.time.LocalDateTime;
+
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Mono;
 
-import java.time.LocalDateTime;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
 
 @Service
 @Slf4j
@@ -24,39 +25,39 @@ public class AccountService {
 
   public Mono<Account> createUser(AccountDTO accountDTO) {
     return repository
-      .findByEmail(accountDTO.getEmail())
-      .defaultIfEmpty(new Account())
-      .flatMap(
-        account -> {
-          if (account.getId() != null) {
-            return Mono.error(new RuntimeException("该邮箱已注册"));
-          }
-          String passwordHash = BCrypt.hashpw(accountDTO.getPassword(), BCrypt.gensalt(10));
-          account = new Account(accountDTO.getName(), accountDTO.getEmail(), passwordHash);
-          return repository.save(account);
-        });
+        .findByEmail(accountDTO.getEmail())
+        .defaultIfEmpty(new Account())
+        .flatMap(
+            account -> {
+              if (account.getId() != null) {
+                return Mono.error(new RuntimeException("该邮箱已注册"));
+              }
+              String passwordHash = BCrypt.hashpw(accountDTO.getPassword(), BCrypt.gensalt(10));
+              account = new Account(accountDTO.getName(), accountDTO.getEmail(), passwordHash);
+              return repository.save(account);
+            });
   }
 
   public Mono<Account> updateUser(AccountDTO accountDTO) {
     return repository
-      .findById(accountDTO.getId())
-      .map(
-        account -> {
-          String password = accountDTO.getPassword();
+        .findById(accountDTO.getId())
+        .map(
+            account -> {
+              String password = accountDTO.getPassword();
 
-          account.setName(accountDTO.getName());
-          account.setEmail(accountDTO.getEmail());
-          account.setTimePerWeek(accountDTO.getTimePerWeek());
-          account.setUpdateTime(LocalDateTime.now());
+              account.setName(accountDTO.getName());
+              account.setEmail(accountDTO.getEmail());
+              account.setTimePerWeek(accountDTO.getTimePerWeek());
+              account.setUpdateTime(LocalDateTime.now());
 
-          if (password != null) {
-            String passwordHash = BCrypt.hashpw(password, BCrypt.gensalt(10));
-            account.setPasswordHash(passwordHash);
-          }
+              if (password != null) {
+                String passwordHash = BCrypt.hashpw(password, BCrypt.gensalt(10));
+                account.setPasswordHash(passwordHash);
+              }
 
-          return account;
-        })
-      .flatMap(repository::save);
+              return account;
+            })
+        .flatMap(repository::save);
   }
 
   public Mono<Void> deleteUser(Integer id) {
@@ -71,6 +72,7 @@ public class AccountService {
   }
 
   public Mono<Boolean> checkPassword(Integer id, String password) {
-    return repository.findById(id).map(Account::getPasswordHash).map(passwordHash -> BCrypt.checkpw(password, passwordHash));
+    return repository.findById(id).map(Account::getPasswordHash)
+        .map(passwordHash -> BCrypt.checkpw(password, passwordHash));
   }
 }
